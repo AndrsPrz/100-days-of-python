47f2c2b7a1f1ae8f74fef280ac35f464f052228a
Davo Lpz <39226783+AndrsPrz@users.noreply.github.com>
2026-03-17 15:36:43 -0500

    Day 09: Blind Auction

---
+++ day_09/blind_auction/main.py
@@ -0,0 +1,29 @@
+import os
+from art import logo
+
+print(logo)
+
+def ask_for_bid(bids):
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bids[name] = bid  # modifies the dict that was passed in
+
+def find_winner(bids):
+    highest_bid = 0
+    winner = ""
+    for name in bids:
+        if bids[name] > highest_bid:
+            highest_bid = bids[name]
+            winner = name
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+bids = {}
+another_bidder = 'yes'
+
+while another_bidder == 'yes':
+    ask_for_bid(bids)
+    another_bidder = input("Are there any other bidders? Type 'yes' or 'no': ")
+    os.system('clear')
+
+if another_bidder == 'no':
+    find_winner(bids)
