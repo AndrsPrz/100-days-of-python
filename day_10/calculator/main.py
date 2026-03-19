b2aa067a49dcaf97902f33ea3e7d5693fd36d6d3
Davo Lpz <39226783+AndrsPrz@users.noreply.github.com>
2026-03-19 10:30:50 -0500

    Refactor calculator and implement operation selection
    Refactored calculator functions and added operation selection.

--- day_10/calculator/main.py
+++ day_10/calculator/main.py
@@ -1,7 +1,11 @@
+import art
+
+def add(n1, n2):
+    return n1 + n2
 def add(n1, n2):
     return n1 + n2
 
-def substract(n1, n2):
+def subtract(n1, n2):
     return n1 - n2
 
 def multiply(n1, n2):
@@ -10,3 +14,33 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+def calculate(operation):
+    operation = operations[operation]
+    return operation(first_number, next_number)
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print(art.logo)
+
+first_number = float(input("What's the first number?: "))
+for operation in operations:
+    print(operation)
+
+def choose_operation():
+    operation = input("Pick an operation: ")
+    next_number = float(input("What's the next number?: "))
+
+choose_operation()
+
+result = print(f"{first_number} {operation} {next_number} = {calculate(operation)}")
+
+restart = input(f"Type 'y' to continue calculating with {calculate(operation)}, or type 'n' to start a new calculation")
+if restart == "y":
+    first_number = calculate(operation)
+    choose_operation()
+    calculate(operation)
