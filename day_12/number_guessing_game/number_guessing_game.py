d0f8dbaa5e0eecc3bd7bf1326b21848b7f4b6e4d
Davo Lpz <39226783+AndrsPrz@users.noreply.github.com>
2026-04-12 11:21:13 -0500

    Refactor number guessing game logic comments

--- day_12/number_guessing_game/number_guessing_game.py
+++ day_12/number_guessing_game/number_guessing_game.py
@@ -1,12 +1,11 @@
-# Number Guessing Game Logic goes here
 #1. Print logo
 #2. Print welcome and ad
 #3. Use random to chose one number
 #4. print choose a difficulty
 #5. function for 'easy' (10 attemps) and 'hard' level, (5 attemps)
 #5.1. Ask a guessing number
-#5.1.1 check if guessing number is a int valid value, if not, print The input is not valid, please enter a number. and back to the step 5.1.
+#5.1.1 check if guessing number is a int valid value by 'try' if yes, continue with the function  if not except: print The input is not valid, please enter a number. and back to the step 5.1.
 #5.2. If guessing number is high print too high and if it is low print too low.
-#5.3. Check the remain opportunities according to the level choosen and using an for loop with the range of the attemps to ask for another guessing number or end the program as lose.
-#5.4. if the number is right. Print Won! and end the program.
-#7. Ask if the customer wish to play again. if the customer write 'yes' back the program to the step 1. so encapsulate the program in While loop to_play. if the customer choose 'no' end program.
+#5.3. Check the remain opportunities according to the level choosen and using an while loop with substraction for a the name of the attemps to ask for another guessing number or end the program as lose.
+#5.4. if the number is right. Print Won! and end the program breack while loop and go out the funtion.
+#7. Ask if the customer wish to play again. if the customer write 'yes' back the program to the step 1. so encapsule the program in While loop to_play. if the customer choose 'no' end program.
