dc1539f1421fde2a434b0c3dbfcec7a465fb19c3
Davo Lpz <39226783+AndrsPrz@users.noreply.github.com>
2026-06-02 21:40:15 -0500

    Document game logic in Higher lower Game Project
    Add game logic comments for the Higher Lower game.

--- day_14/Higher lower Game Project/Higher lower Game Project.py
+++ day_14/Higher lower Game Project/Higher lower Game Project.py
@@ -1,1 +1,12 @@
-
+#Add logo from the Art.py file and print it 
+#Print Compare A: 
+#chose a ramdong dictiorary from a list, print name, a profession and country 
+#Save the follower amount on a variable calle followersA
+# print VS logo
+#Print Against B: Name, profession and Country
+#Safe followers amount on FollowersB
+# Then imput "Who has more followers? Type 'A' or 'B':"
+#Take the input and save it on a variable called guessing. 
+# Compare FollowersA and FollowersB and take the higher on variable Higher_Followers and safe there the letter 
+# If Guessing is == to Higher_Followers print clien console and display the game again but fist create  print Score and add it 1 point. 
+# Else Print Wrong answer. Gameover. 
