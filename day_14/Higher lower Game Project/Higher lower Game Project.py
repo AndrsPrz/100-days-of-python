b36a742ecd4dc11b87cecf0b46d2b4cc79f430c6
Davo Lpz <39226783+AndrsPrz@users.noreply.github.com>
2026-06-04 15:44:24 -0500

    Update Higher lower Game Project.py

--- day_14/Higher lower Game Project/Higher lower Game Project.py
+++ day_14/Higher lower Game Project/Higher lower Game Project.py
@@ -1,12 +1,21 @@
 #Add logo from the Art.py file and print it 
+# Create a variable called score = 0
 #Print 'Compare A:' 
-#chose a ramdon element from data dictiorary, then print name, a profession and country 
-#Save the follower amount on a variable calle followersA
-# print VS logo
-#Print Against B: Name, profession and Country
-#Safe followers amount on FollowersB
-# Then imput "Who has more followers? Type 'A' or 'B':"
-#Take the input and save it on a variable called guessing. 
-# Compare FollowersA and FollowersB and take the higher on variable Higher_Followers and safe there the letter 
-# If Guessing is == to Higher_Followers print clien console and display the game again but fist create  print Score and add it 1 point. 
+#From game_data.py file, chose a ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
+#Save the follower_count on a variable calle A_followers
+# print 'VS' logo
+#Print 'Against B:'
+#From game_data.py file, chose another ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
+#Safe followers amount on B_followers
+#Create a variable called the_highest
+# Make a conditional if A_followers > B_followers so, the_highest = A_followers, else the_highest = B_followers
+# Then try to print the imput "Who has more followers? Type 'A' or 'B':", save it in 'guessing' variable and save guessing.lower() in a new variable called 'guessing_highest' .
+# Compare the_highest == guessing_highest 
+# If the previous condition is tru, first score += 1, then delete full the previos print. We can use   import os
+
+# def clear_screen():
+#     os.system('cls' if os.name == 'nt' else 'clear')
+# After logo clear_screen() and print 'You're right'
+# print cln console and display the game again but fist create  print Score and add it 1 point. }
+
 # Else Print Wrong answer. Gameover. 
