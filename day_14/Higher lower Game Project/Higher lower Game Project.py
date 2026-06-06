099389e19db9fcfd9b8b75346cd68c5a823b1e63
Davo Lpz <39226783+AndrsPrz@users.noreply.github.com>
2026-06-05 22:41:09 -0500

    Organize game logic into main function with loop
    Refactor game logic into a main function and implement a while loop for continuous play.

--- day_14/Higher lower Game Project/Higher lower Game Project.py
+++ day_14/Higher lower Game Project/Higher lower Game Project.py
@@ -1,21 +1,22 @@
 #Add logo from the Art.py file and print it 
 # Create a variable called score = 0
-#Print 'Compare A:' 
-#From game_data.py file, chose a ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
-#Save the follower_count on a variable calle A_followers
-# print 'VS' logo
-#Print 'Against B:'
-#From game_data.py file, chose another ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
-#Safe followers amount on B_followers
-#Create a variable called the_highest
-# Make a conditional if A_followers > B_followers so, the_highest = A_followers, else the_highest = B_followers
-# Then try to print the imput "Who has more followers? Type 'A' or 'B':", save it in 'guessing' variable and save guessing.lower() in a new variable called 'guessing_highest' .
-# Compare the_highest == guessing_highest 
-# If the previous condition is tru, first score += 1, then delete full the previos print. We can use   import os
-
-# def clear_screen():
-#     os.system('cls' if os.name == 'nt' else 'clear')
-# After logo clear_screen() and print 'You're right'
-# print cln console and display the game again but fist create  print Score and add it 1 point. }
-
-# Else Print Wrong answer. Gameover. 
+# get into in all in a main function:
+ # keep playing with a while loop, while play_game = true 
+  #Print 'Compare A:' 
+  #From game_data.py file, chose a ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
+  #Save the follower_count on a variable calle A_followers
+  # print 'VS' logo
+  #Print 'Against B:'
+  #From game_data.py file, chose another ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
+  #Safe followers amount on B_followers
+  #Create a variable called the_highest
+  # Make a conditional if A_followers > B_followers so, the_highest = a, else the_highest = b
+  # Then try to print the imput "Who has more followers? Type 'A' or 'B':", save it in 'guessing' variable and save guessing.lower() in a new variable called 'guessing_highest' .
+  # Compare the_highest == guessing_highest 
+  # If the previous condition is true, first score += 1, then delete full the previos print. We can use   import os
+    # def clear_screen():
+    #     os.system('cls' if os.name == 'nt' else 'clear')
+    # After logo clear_screen() and print 'You're right! Current score: {score}'
+    # print in the console and display the game again but fist create  print Score and add it 1 point, and the right answer a or b, woud be the new A value. So save the right answer items in a.
+  # however If the answer is wrong, it's guessing is not equal to the_highest finish the program, pay_game = false and print 'Sorry, that's wrong. Final score: 0'
+#  Gameover.
