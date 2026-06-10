e6fdcec747260e989769e589d41dc67a07929bb9
Davo Lpz <39226783+AndrsPrz@users.noreply.github.com>
2026-06-09 22:27:35 -0500

    Fix comments for clarity in game logic

--- day_14/Higher lower Game Project/Higher lower Game Project.py
+++ day_14/Higher lower Game Project/Higher lower Game Project.py
@@ -4,16 +4,18 @@
 # get into in all in a main function:
  # keep playing with a while loop, 
   #Print 'Compare A:' 
-  #From game_data.py file, chose a ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
+  #From game_data.py file, chose a ramdon dictionary from the list called 'data', then from the dictionary choosen and print name, a 'description' and 'country'
   #Save the follower_count on a variable calle A_followers
   # print 'VS' logo
   #Print 'Against B:'
   #From game_data.py file, chose another ramdon dictionary from the list called 'data', then from the dictionary choose and print name, a 'description' and 'country'
   #Safe followers amount on B_followers
   #Create a variable called the_highest
-  # Make a conditional if A_followers > B_followers so, the_highest = a, else the_highest = b
+
+  # Make a conditional if A_followers > B_followers so, the_highest = dictinary of data choosen on printed on A option, else the_highest = dictinary of data choosen on printed on B option
   # Then try to print the imput "Who has more followers? Type 'A' or 'B':", save it in 'guessing' variable and save guessing.lower() in a new variable called 'guessing_highest' .
-  # Compare the_highest == guessing_highest 
+  # Compare the_highest.followers == guessing_highest -> take the followers amount into dictionary positon, that is into the list into the data file. 
+
   # If the previous condition is true, first score += 1, then delete full the previos print. We can use   import os
     # def clear_screen():
     #     os.system('cls' if os.name == 'nt' else 'clear')
